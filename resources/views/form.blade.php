4d6c15eba8a352d965919846c9ff935b52e4aeac
Tiago Rodrigues dos Santos <tiago.rodrigues@academico.ifrn.edu.br>
2022-11-24 11:52:27 -0300

    add small tags

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -13,16 +13,16 @@
 </head>
 <body class="vstack align-items-center justify-content-center">
     <div class="container col-12 col-sm-8 col-lg-6 col-xl-4">
-        <form action="/calcular" method="post" class="border border-2 border-dark border-opacity-10 rounded-3 p-4">
+        <form action="/calcular" method="post" class="border border-2 border-dark border-opacity-10 rounded-3 p-4" id="form">
             {{ csrf_field() }}
             <h1 class="mb-3">Calculadora de IMC</h1>
             <div class="mb-3">
-                <label for="peso" class="form-label">Peso</label>
-                <input type="text" class="form-control" id="peso" name="peso" placeholder="Seu peso em Kg">
+                <label for="peso" class="form-label">Peso <small>(Kg)</small></label>
+                <input type="text" class="form-control" id="peso" name="peso" placeholder="Exemplo: 60.2">
             </div>
             <div class="mb-3">
-                <label for="altura" class="form-label">Altura</label>
-                <input type="text" class="form-control" id="altura" name="altura" placeholder="Sua altura em m">
+                <label for="altura" class="form-label">Altura <small>(metros)</small></label>
+                <input type="text" class="form-control" id="altura" name="altura" placeholder="Exemplo: 1.80">
             </div>
             <button type="submit" class="w-100 mt-3 btn bg-primary text-white">Calcular</button>
         </form>
