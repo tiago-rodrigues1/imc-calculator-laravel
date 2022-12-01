2c8745b7b4c068d4bf86cdd14c64721b533612da
tiago-rodrigues1 <tiagords127@gmail.com>
2022-12-01 11:49:29 -0300

    add template

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,33 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de IMC</title>
+@extends('templates.template_principal')
 
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+@section('titulo', 'Home')
 
-    <link rel="stylesheet" href="/styles/bootstrap.min.css">
-    <link rel="stylesheet" href="/styles/globals.css">
-</head>
-<body class="vstack align-items-center justify-content-center">
-    <div class="container col-12 col-sm-8 col-lg-6 col-xl-4">
-        <form action="/calcular" method="post" class="border border-2 border-dark border-opacity-10 rounded-3 p-4" id="form">
-            {{ csrf_field() }}
-            <h1 class="mb-3">Calculadora de IMC</h1>
-            <div class="mb-3">
-                <label for="peso" class="form-label">Peso <small>(Kg)</small></label>
-                <input type="text" class="form-control" id="peso" name="peso" placeholder="Exemplo: 60.2">
-            </div>
-            <div class="mb-3">
-                <label for="altura" class="form-label">Altura <small>(metros)</small></label>
-                <input type="text" class="form-control" id="altura" name="altura" placeholder="Exemplo: 1.80">
-            </div>
-            <button type="submit" class="w-100 mt-3 btn bg-primary text-white">Calcular</button>
-        </form>
-    </div>
-
-    <script src="/scripts/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+@section('conteudo')
+    <form action="/calcular" method="post" class="border border-2 border-dark border-opacity-10 rounded-3 p-4" id="form">
+        {{ csrf_field() }}
+        <h1 class="mb-3">Calculadora de IMC</h1>
+        <div class="mb-3">
+            <label for="peso" class="form-label">Peso <small>(Kg)</small></label>
+            <input type="text" class="form-control" id="peso" name="peso" placeholder="Exemplo: 60.2">
+        </div>
+        <div class="mb-3">
+            <label for="altura" class="form-label">Altura <small>(metros)</small></label>
+            <input type="text" class="form-control" id="altura" name="altura" placeholder="Exemplo: 1.80">
+        </div>
+        <button type="submit" class="w-100 mt-3 btn bg-primary text-white">Calcular</button>
+    </form>
+@endsection
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -1,43 +1,30 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de IMC</title>
+@extends('templates.template_principal')
 
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+@section('titulo', 'Resultado')
+@section('classe', 'text-center')
 
-    <link rel="stylesheet" href="/styles/bootstrap.min.css">
-    <link rel="stylesheet" href="/styles/globals.css">
-</head>
-<body class="vstack align-items-center justify-content-center">
-    <div class="container col-12 col-sm-8 col-lg-6 col-xl-4 text-center">
-        <h1 class="fw-bold">Seu IMC é {{ number_format($imc, 2, ",", ".") }}</h1>
-        <hr>
-        <h2 class="mt-4">
-            @if ($imc < 18.5)
-                Você está abaixo do peso
+@section('conteudo')
+    <h1 class="fw-bold">Seu IMC é {{ number_format($imc, 2, ",", ".") }}</h1>
+    <hr>
+    <h2 class="mt-4">
+        @if ($imc < 18.5)
+            Você está abaixo do peso
 
-            @elseif ($imc < 24.9)
-                Você está no peso ideal
+        @elseif ($imc < 24.9)
+            Você está no peso ideal
 
-            @elseif ($imc < 29.9)
-                Você está levemente acima do peso
+        @elseif ($imc < 29.9)
+            Você está levemente acima do peso
 
-            @elseif ($imc < 34.9)
-                Você está com obesidade grau I
+        @elseif ($imc < 34.9)
+            Você está com obesidade grau I
 
-            @elseif ($imc <= 39.9)
-                Você está com obesidade grau II (severa)
+        @elseif ($imc <= 39.9)
+            Você está com obesidade grau II (severa)
 
-            @else
-                Você está com obesidade grau III (mórbido)
-            @endif
-        </h2>
-
-        <a href="/" class="col-2 mt-5 btn ghost text-primary">Voltar</a>
-    </div>
-    <script src="/scripts/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+        @else
+            Você está com obesidade grau III (mórbido)
+        @endif
+    </h2>
+    <a href="/" class="col-2 mt-5 btn ghost text-primary">Voltar</a>
+@endsection
